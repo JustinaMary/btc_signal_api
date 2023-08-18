a2c0fc31f243afedb42dcb4169f02f9aad47b6de
Justina <justinamary27@gmail.com>
2023-08-18 14:08:24 +0530

    changes in formatting

--- utils/live_trading.py
+++ utils/live_trading.py
@@ -57,66 +57,64 @@ def luke():
             signal_value = 'buy'
             #order_size_usd = ((equity * candle_mean / current_close) - equity) * 1.2
             #order_size_base = order_size_usd / current_close
-
         elif current_rate > funding_upper_bb and current_close > candle_upper_bb and current_close > long_candle_mean:
             print('sell signal')
             signal_value = 'sell'
             #order_size_usd = ((equity * candle_mean / current_close) - equity)
             #order_size_base = order_size_usd / current_close
-
         else:
             print('waiting for next signal. previous signals: ')
             signal_value = 'waiting for next'
             # remaining time until next signal
-            now_epoch = int(epoch.time())
-            next_signal_time = ((candles_reverse[0]['close_time'] / 1000) - now_epoch)
+        now_epoch = int(epoch.time())
+        next_signal_time = ((candles_reverse[0]['close_time'] / 1000) - now_epoch)
 
-            # data
-            funding_df = pd.DataFrame(funding)
-            ohlc_df = pd.DataFrame(candles)
+        # data
+        funding_df = pd.DataFrame(funding)
+        ohlc_df = pd.DataFrame(candles)
 
-            # funding data
+        # funding data
 
-            # time
-            funding_df['hr_time'] = pd.to_datetime(funding_df['timestamp'], unit='ms')
-            funding_df['hr_time'] = funding_df['hr_time'].dt.round('S')
-
-            # bbs
-            funding_df['mid_bb'] = funding_df['funding_rate'].rolling(window=repo.const_mean_length).mean()
-            funding_df['funding_std'] = funding_df['funding_rate'].rolling(window=repo.const_mean_length).std()
-            funding_df['upper_bb'] = funding_df['mid_bb'] + (2 * funding_df['funding_std'])
-            funding_df['lower_bb'] = funding_df['mid_bb'] - (2 * funding_df['funding_std'])
-
-            # ohlc data
-
-            # time
-            ohlc_df['hr_open_time'] = pd.to_datetime(ohlc_df['open_time'], unit='ms')
-            ohlc_df['hr_open_time'] = ohlc_df['hr_open_time'].dt.round('S')
-            ohlc_df['hr_close_time'] = pd.to_datetime(ohlc_df['close_time'], unit='ms')
-            ohlc_df['hr_close_time'] = ohlc_df['hr_close_time'].dt.round('S')
+        # time
+        funding_df['hr_time'] = pd.to_datetime(funding_df['timestamp'], unit='ms')
+        funding_df['hr_time'] = funding_df['hr_time'].dt.round('S')
 
             # bbs
-            ohlc_df['mid_bb'] = ohlc_df['close'].rolling(window=repo.const_mean_length).mean()
-            ohlc_df['ohlc_std'] = ohlc_df['close'].rolling(window=repo.const_mean_length).std()
-            ohlc_df['upper_bb'] = ohlc_df['mid_bb'] + (2 * ohlc_df['ohlc_std'])
-            ohlc_df['lower_bb'] = ohlc_df['mid_bb'] - (2 * ohlc_df['ohlc_std'])
+        funding_df['mid_bb'] = funding_df['funding_rate'].rolling(window=repo.const_mean_length).mean()
+        funding_df['funding_std'] = funding_df['funding_rate'].rolling(window=repo.const_mean_length).std()
+        funding_df['upper_bb'] = funding_df['mid_bb'] + (2 * funding_df['funding_std'])
+        funding_df['lower_bb'] = funding_df['mid_bb'] - (2 * funding_df['funding_std'])
 
-            # long ma
-            ohlc_df['200sma'] = ohlc_df['close'].rolling(window=repo.const_long_mean_length).mean()
+        # ohlc data
 
-            # signals
-            buy_signal = (ohlc_df['close'] < ohlc_df['lower_bb']) & (funding_df['funding_rate'] < funding_df['lower_bb'])
-            sell_signal = (ohlc_df['close'] > ohlc_df['upper_bb']) & (funding_df['funding_rate'] > funding_df['upper_bb']) & (ohlc_df['close'] > ohlc_df['200sma'])
+        # time
+        ohlc_df['hr_open_time'] = pd.to_datetime(ohlc_df['open_time'], unit='ms')
+        ohlc_df['hr_open_time'] = ohlc_df['hr_open_time'].dt.round('S')
+        ohlc_df['hr_close_time'] = pd.to_datetime(ohlc_df['close_time'], unit='ms')
+        ohlc_df['hr_close_time'] = ohlc_df['hr_close_time'].dt.round('S')
 
-            # buy signal
-            filtered_buy_signal_funding = funding_df[buy_signal]
-            filtered_buy_signal_ohlc = ohlc_df[buy_signal]
+        # bbs
+        ohlc_df['mid_bb'] = ohlc_df['close'].rolling(window=repo.const_mean_length).mean()
+        ohlc_df['ohlc_std'] = ohlc_df['close'].rolling(window=repo.const_mean_length).std()
+        ohlc_df['upper_bb'] = ohlc_df['mid_bb'] + (2 * ohlc_df['ohlc_std'])
+        ohlc_df['lower_bb'] = ohlc_df['mid_bb'] - (2 * ohlc_df['ohlc_std'])
 
-            print('buy signals: ')
-            for index, row in filtered_buy_signal_ohlc.iterrows():
-                time = row['hr_open_time']
+        # long ma
+        ohlc_df['200sma'] = ohlc_df['close'].rolling(window=repo.const_long_mean_length).mean()
 
-                close = row['close']
+        # signals
+        buy_signal = (ohlc_df['close'] < ohlc_df['lower_bb']) & (funding_df['funding_rate'] < funding_df['lower_bb'])
+        sell_signal = (ohlc_df['close'] > ohlc_df['upper_bb']) & (funding_df['funding_rate'] > funding_df['upper_bb']) & (ohlc_df['close'] > ohlc_df['200sma'])
+
+        # buy signal
+        filtered_buy_signal_funding = funding_df[buy_signal]
+        filtered_buy_signal_ohlc = ohlc_df[buy_signal]
+
+        print('buy signals: ')
+        for index, row in filtered_buy_signal_ohlc.iterrows():
+            time = row['hr_open_time']
+
+            close = row['close']
                 #order_size_usd = ((equity * (row['mid_bb'] / close)) - equity) * 1.2
                 #order_size_base = order_size_usd / close
 
@@ -126,15 +124,15 @@ def luke():
 
                 #print(f'time: {time}, price: {close}, order size: {order_size_usd}, average entry: {average_entry}')
 
-            # sell signal
-            filtered_sell_signal_funding = funding_df[sell_signal]
-            filtered_sell_signal_ohlc = ohlc_df[sell_signal]
+        # sell signal
+        filtered_sell_signal_funding = funding_df[sell_signal]
+        filtered_sell_signal_ohlc = ohlc_df[sell_signal]
 
-            print('\nsell signals: ')
-            for index, row in filtered_sell_signal_ohlc.iterrows():
-                time = row['hr_open_time']
+        print('\nsell signals: ')
+        for index, row in filtered_sell_signal_ohlc.iterrows():
+            time = row['hr_open_time']
 
-                close = row['close']
+            close = row['close']
                 #order_size_usd = ((equity * (close / row['mid_bb'])) - equity)
                 #order_size_base = order_size_usd / close
 
@@ -144,12 +142,12 @@ def luke():
 
                 #print(f'time: {time}, price: {close}, order size: {order_size_usd}, average entry: {average_exit}')
 
-            print('\ncurrent position: ')
+        print('\ncurrent position: ')
 
             #position_size = sum(buy_volume) - sum(sell_volume)
             #average_entry = sum(buy_volume) / sum(buy_size)
             #average_exit = sum(sell_volume) / sum(sell_size)
-            price = ohlc_df.iloc[-1]['close']
+        price = ohlc_df.iloc[-1]['close']
             #sum_open = sum(buy_size) * price
             #sum_exit = sum(sell_size) * price
             # realized_profit = ((average_exit / average_entry) * sum_exit) - sum_exit
@@ -176,14 +174,14 @@ def luke():
             #     'message' : message,
             #     'remaining_time': remaining_time
             # }
-            remaining_time = round(next_signal_time / 3600)
-            position_dict = {
-                'price': price,
-                'signal': signal_value,
-                'remaining_time': remaining_time
-            }
+        remaining_time = round(next_signal_time / 3600)
+        position_dict = {
+            'price': price,
+            'signal': signal_value,
+            'remaining_time': remaining_time
+        }
 
-            return position_dict 
+        return position_dict 
     
             # if price > average_entry:
             #     print(f'Congratulations. You are in ${round(realized_profit + unrealized_profit, 2)} of profits. Or %{round((((equity + realized_profit + unrealized_profit) / equity) - 1) * 100, 2)} returns.')
